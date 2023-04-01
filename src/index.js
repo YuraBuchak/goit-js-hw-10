817f72cede5f402b1e6f06a63d30e473e4b8666b
YuraBuchak <115108617+YuraBuchak@users.noreply.github.com>
2023-04-01 20:53:30 +0300

    remark

--- src/index.js
+++ src/index.js
@@ -29,46 +29,41 @@ const handleSerchCountry = event => {
   fetchCountries(valueInput)
     .then(arrays => {
       if (arrays.length > 10) {
-        // console.log('Too many matches found. Please enter a more specific name.');
         Notiflix.Notify.info(
           'Too many matches found. Please enter a more specific name.'
         );
         listEl.innerHTML = '';
         divEl.innerHTML = '';
-        return;
-      }
-
-      if ((arrays.length > 1) & (arrays.length <= 10)) {
-        fetchCountries(valueInput).then(arrayOfObjs => {
-          const markUpListCountry = arrayOfObjs
-            .map(
-              array =>
-                `<li><img src="${array.flags.png}" width="20" height="20"> <span style="font-size: 22px; font-weight: bold;">${array.name.common}</span></li>`
-            )
-            .join('');
-          divEl.innerHTML = '';
-          listEl.innerHTML = markUpListCountry;
-        });
-        return;
-      }
-
-      if (arrays.length === 1) {
-        fetchCountries(valueInput).then(arrays => {
-          const { flags, languages, name, population, capital } = arrays[0];
-
-          listEl.innerHTML = '';
-          divEl.innerHTML = `<ul style="list-style: none;">
-        <h1><img src="${flags.png}" width="25" height="25"> <span>${
-            name.common
-          }</span></h1>
-        <li><span style="font-weight: bold;">Capital: </span>${capital}</li>
+      } else if (arrays.length > 1 && arrays.length <= 10) {
+        const markUpListCountry = arrays
+          .map(
+            array =>
+              `<li><img src="${array.flags.png}" width="20" height="20"> <span style="font-size: 22px; font-weight: bold;">${array.name.common}</span></li>`
+          )
+          .join('');
+        divEl.innerHTML = '';
+        listEl.innerHTML = markUpListCountry;
+      } else if (arrays.length === 1) {
+        const markUpInfoCountry = arrays
+          .map(
+            array => `<ul style="list-style: none;">
+        <h1><img src="${array.flags.png}" width="25" height="25"> <span>${
+              array.name.common
+            }</span></h1>
+        <li><span style="font-weight: bold;">Capital: </span>${
+          array.capital
+        }</li>
         <li><span style="font-weight: bold;">Languages: </span>${Object.values(
-          languages
+          array.languages
         )}</li>
-        <li><span style="font-weight: bold;">Population: </span>${population}</li>
-        </ul>`;
-        });
-        return;
+        <li><span style="font-weight: bold;">Population: </span>${
+          array.population
+        }</li>
+        </ul>`
+          )
+          .join('');
+        listEl.innerHTML = '';
+        divEl.innerHTML = markUpInfoCountry;
       }
     })
     .catch(Error => {
